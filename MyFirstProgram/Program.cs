cf01ef2423a1593957479fed83ee638d351804c2
tharle <tharles.josefidecamargo@gmail.com>
2022-02-28 11:54:18 -0500

    F to C funtion

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -2,11 +2,19 @@
 
 namespace MyFirstProgram
 {
+    class Temperature 
+    {
+        public static float FahrenheiToCelsius(float temperatureF)
+        {
+            return (temperatureF - 32) / 1.8f;
+        }
+    }
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! " + Temperature.FahrenheiToCelsius(350));
         }
     }
 }
