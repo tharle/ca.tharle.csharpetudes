81b66935323695c46786cafd450ef00a2b60311b
tharle <tharles.josefidecamargo@gmail.com>
2022-03-09 12:51:27 -0500

    add Generic Repository

---
+++ C# Design Patterns Data Access/02/MyShop.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace MyShop.Infrastructure.Repositories
+{
+    public abstract class GenericRepository<T> : IRepository<T> where T: class
+    {
+        protected ShoppingContext context;
+
+        public GenericRepository(ShoppingContext context)
+        {
+            this.context = context;
+        }
+
+        public virtual T Add(T entity)
+        {
+            return context.Add(entity).Entity;
+        }
+
+        public virtual IEnumerable<T> All()
+        {
+            return context.Set<T>()
+                          .ToList();
+        }
+
+        public virtual IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return context.Set<T>()
+                         .AsQueryable()
+                         .Where(predicate)
+                         .ToList();
+        }
+
+        public virtual T Get(Guid id)
+        {
+            return context.Find<T>(id);
+        }
+
+        public virtual void SaveChanges()
+        {
+            context.SaveChanges();
+        }
+
+        public virtual T Update(T entity)
+        {
+            return context.Update<T>(entity).Entity;
+        }
+    }
+}
---
+++ C# Design Patterns Data Access/02/MyShop.Infrastructure/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace MyShop.Infrastructure.Repositories
+{
+    public interface IRepository<T>
+    {
+        T Add(T entity);
+        T Update(T entity);
+        T Get(Guid id);
+        IEnumerable<T> All();
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+        void SaveChanges();
+    }
+}
