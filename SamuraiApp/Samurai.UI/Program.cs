0cba255dc919b8d2dfa79304fdcea54f52419262
tharle <tharles.josefidecamargo@gmail.com>
2022-03-10 13:00:01 -0500

    EF core 5 start base donnes

--- SamuraiApp/Samurai.UI/Program.cs
+++ SamuraiApp/Samurai.UI/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System;
+using System.Linq;
 
 namespace SamuraiApp.UI
 {
     class Program
     {
-        static void Main(string[] args)
+        private static SamuraiContext _context = new SamuraiContext();
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _context.Database.EnsureCreated();
+            GetSamurais("Before Add: ");
+            AddSamurai();
+            GetSamurais("After Add: ");
+            Console.WriteLine("Press any key...");
+            Console.ReadLine();
+        }
+
+        private static void AddSamurai()
+        {
+            var samurai = new Samurai { Name = "Tharle" };
+            _context.Samurais.Add(samurai);
+            _context.SaveChanges();
+        }
+
+        private static void GetSamurais(string text)
+        {
+            var samurais = _context.Samurais.ToList();
+            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+            foreach (var samurai in samurais)
+            {
+                Console.WriteLine(samurai.Name);
+            }
         }
     }
 }
--- SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -13,5 +13,9 @@ namespace SamuraiApp.Data
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source= (localdb)\\MSSQLLocalDB; Initial Catalog=SamuraiAppData");
+        }
     }
 }
