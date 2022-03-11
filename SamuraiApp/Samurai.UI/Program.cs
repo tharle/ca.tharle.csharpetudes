37fbeaa394896d1d4d4ef44c975623c290b68241
tharle <tharles.josefidecamargo@gmail.com>
2022-03-11 14:22:59 -0500

    Update data

--- SamuraiApp/Samurai.UI/Program.cs
+++ SamuraiApp/Samurai.UI/Program.cs
@@ -15,8 +15,10 @@ namespace SamuraiApp.UI
             _context.Database.EnsureCreated();
             // AddSamuraiByName("Shimada", "Okamoto", "Kikuchio", "Hyashida");
             // GetSamurais("After Add: ");
-            //GetFirstSamuraiByNameLike("S");
-            GetSamuraiById(1);
+            // GetFirstSamuraiByNameLike("S");
+            // GetSamuraiById(1);
+            // RetriveAndUpdateSamurai(1, "-san");
+            RetriveAndUpdateMultiplesSamurai("-san");
             Console.WriteLine("Press any key...");
             Console.ReadLine();
         }
@@ -30,7 +32,6 @@ namespace SamuraiApp.UI
             
             _context.SaveChanges();
         }
-
         private static void GetSamurais(string text)
         {
             var samurais = _context.Samurais.ToList();
@@ -40,7 +41,6 @@ namespace SamuraiApp.UI
                 Console.WriteLine(samurai.Name);
             }
         }
-
         private static void GetSamuraisByNameLike(String name)
         {
             // var samurais = _context.Samurais.Where(s => s.Name == name).ToList();
@@ -51,21 +51,45 @@ namespace SamuraiApp.UI
                 Console.WriteLine(samurai.Name);
             }
         }
-
         private static void GetFirstSamuraiByNameLike(String name)
         {
             // var samurais = _context.Samurais.Where(s => s.Name == name).ToList();
             var samurai = _context.Samurais.FirstOrDefault(s => EF.Functions.Like(s.Name, $"{name}%"));
             Console.WriteLine(samurai.Name);
         }
-
         private static void GetSamuraiById(int idSamurai)
         {
             // var samurais = _context.Samurais.Where(s => s.Name == name).ToList();
             var samurai = _context.Samurais.Find(idSamurai);
             Console.WriteLine(samurai.Name);
         }
+        private static void RetriveAndUpdateSamurai(int idSamurai, string prefixName)
+        {
+            var samurai  = _context.Samurais.Find(idSamurai);
+            verifyAndModifySamuraiPrefixName(samurai, prefixName);
+            _context.SaveChanges();
+        }
 
-        
+        private static void verifyAndModifySamuraiPrefixName(Samurai samurai, string prefixName)
+        {
+            if (samurai.Name.Contains(prefixName))
+            {
+                Console.WriteLine($"{samurai.Name} contains {prefixName}, then remove-it!");
+                samurai.Name = samurai.Name.Replace(prefixName, "");
+            }
+            else
+            {
+                Console.WriteLine($"{samurai.Name} not contains {prefixName}, then add!");
+                samurai.Name += prefixName;
+            }
+        }
+        private static void RetriveAndUpdateMultiplesSamurai(string prefixName)
+        {
+            var samurais = _context.Samurais.Skip(1).Take(4).ToList();
+            samurais.ForEach(s => verifyAndModifySamuraiPrefixName(s, prefixName));
+            _context.SaveChanges();
+        }
+
+
     }
 }
