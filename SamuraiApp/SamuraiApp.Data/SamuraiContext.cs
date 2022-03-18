4c1b38318064c64319f450ac74366fe043763bd6
tharle <tharles.josefidecamargo@gmail.com>
2022-03-18 14:27:12 -0400

    Using KeyLess entities to map to views

--- SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -14,6 +14,7 @@ namespace SamuraiApp.Data
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Battle> Battles { get; set; }
+        public DbSet<SamuraiBattleStat> SamuraiBattleStats { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -39,6 +40,7 @@ namespace SamuraiApp.Data
                 .HasDefaultValueSql("getdate()");
 
             modelBuilder.Entity<Horse>().ToTable("Horses");
+            modelBuilder.Entity<SamuraiBattleStat>().HasNoKey();
                 
         }
     }
---
+++ SamuraiApp/SamuraiApp.Domain/SamuraiBattleStat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiApp.Domain
+{
+    public class SamuraiBattleStat
+    {
+        public string Name { get; set; }
+        public int? NumnerOfBattles { get; set; }
+        public string EarliesBattle { get; set; }
+    }
+}
