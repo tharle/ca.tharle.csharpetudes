8bbf3bc13cb264a0f2a0c38e3c7e5ed709c4263e
tharle <tharles.josefidecamargo@gmail.com>
2022-04-01 09:12:18 -0400

    Data Binding and Pipes

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/products-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
